5c2c98ccb19396399280f9ecebc50af805233a78
Dave Trost <davetrost@gmail.com>
2019-08-17 21:12:42 -0700

    animation updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { App } from './components/pokedex/App.js';
 
+/* pause CSS animations until the page is loaded
+     https://css-tricks.com/making-animations-wait/  */
+document.body.classList.add('js-loading');
+window.addEventListener('load', () => setTimeout(() => {
+    document.body.classList.remove('js-loading');
+}, 100));
+
 const root = document.getElementById('root');
 const app = new App();
 const dom = app.renderDOM();
